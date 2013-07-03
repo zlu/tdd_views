ed5fbc96b5ee16dbc8d39cb468fd1ac65a91b4a7
zlu <zlu@me.com>
2013-07-03 15:22:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddViews::Application.config.secret_token = 'c3f947354076332822e765e9ed396502abc2fa7c80a158e9e83010000c24e1e7a67557bd6a01c760694142885279a6469faf5a0517b3f4df17a458021ba216b5'
---
+++ spec/views/tasks/new.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'tasks/new.html.erb' do
+	let(:task) { mock_model('Task').as_new_record }
+
+	before do
+		assign(:task, task)
+		render
+	end
+
+	it 'displays New Task' do				
+		rendered.should contain 'New Task'
+	end
+
+	it 'displays a create task form' do
+		rendered.should have_selector('form', method: 'post', action: tasks_path) do |form|
+			form.should have_selector('input', type: 'submit')
+		end
+	end
+
+	it 'displays task name field' do
+		rendered.should have_selector('form', method: 'post', action: tasks_path) do |form|
+			form.should have_selector('input', id: 'task_name', name: 'task_name', type: 'text')
+		end
+	end
+end
